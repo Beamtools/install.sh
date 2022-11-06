7987679a4272b2ee04b3365e015426cadc1cfe0b
Beamtools <117571617+Beamtools@users.noreply.github.com>
2022-11-06 18:00:36 +0700

    Add files via upload

---
+++ installed.sh
@@ -0,0 +1,59 @@
+echo "Good install packages"
+"done"
+termux-setup-storage
+pkg update && pkg upgrade
+apt update && apt upgrade
+apt install curl -y
+apt install wget -y
+apt install python -y
+apt install python2 -y
+apt install bash -y
+apt install cowsay -y
+apt install git -y
+apt install dart -y
+apt install toilet -y
+apt install fish -y
+apt install figlet -y
+apt install php -y
+apt install php -y
+apt install ruby -y
+apt install perl -y
+apt install clang -y
+apt install macchanger -y
+apt install jq -y
+apt install tor -y
+pkg install openssh -y
+apt install openssly
+apt-get update -y
+apt-get upgrade -y
+pip install --upgrade pip
+pip install requests
+pip install colorama
+pip install bs4
+pip install sqlmap
+pip2 install requests
+pip2 install colorama
+pip2 install bs4
+pip3 install requests
+pip3 install bs4 
+pip3 install colorama
+pkg install nmap
+pip install lolcat
+pkg install lolcat
+git clone https://github.com/EH4404/freefc
+git clone https://github.com/palahsu/DDoS-Ripper
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+git clone https://github.com/EH4404/ehstore
+git clone https://github.com/Sohil876/Termux-zsh
+git clone https://github.com/h4ck3r0/Termux-os
+git clone https://github.com/KasRoudra/stylishtermux
+apt install zsh -y
+git clone https://github.com/remo7777/T-Header.git
+git clone https://github.com/sabri-zaki/EasY_HaCk
+git clone https://github.com/adi1090x/termux-style
+git clone https://github.com/mishakorzik/AllHackingTools
+apt install toilet figlet exa wget ruby -y
+gem install lolcat 
+dpkg --configure -a
+clear
+
